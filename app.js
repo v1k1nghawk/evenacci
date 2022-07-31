299e8fe6eb4e732c71c57847e1cd230d52729afe
tamar1357 <tamariyaann@gmail.com>
2022-07-31 16:53:53 +0300

    git commit -am 'fibbonacci sequence with javascript'

---
+++ app.js
@@ -0,0 +1,20 @@
+//Объекты  (eto toze tip dannih)
+
+let a =1
+let b = 1
+let c = 2
+function fib(num) {
+ for(let i = 1; i<num;i++)
+ {
+     c = a+b
+     a = b;
+     b = c;
+
+      if((c>>1)<<1 === c)
+         console.log(c)
+ }
+}
+var number = prompt('Enter number:')
+fib(number)
+
+
